2463dad3e426b77ea5cbb49326d7cb4d4caa6870
Suneeragu <sunee.ragu@gmail.com>
2022-02-25 10:36:27 -0600

    message five

--- customCellTableview/ViewController.swift
+++ customCellTableview/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         fruitsTableview.delegate = self
         fruitsTableview.dataSource = self
         print(fruitsArr)
+        print(fruitsArr)
     }
 
 }
